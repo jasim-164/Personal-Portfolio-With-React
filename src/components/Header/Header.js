7d6915a8fe2ce59aa596dda07f53440adf0688b9
jasim-164 <jasimuddincse1703164@gmail.com>
2022-06-16 08:17:15 +0600

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className=" ">
-            <div >
-            <h1>Jasim's portfolio</h1>
-            </div>
-            <div>
-            <Link to='/about'>about</Link>
-            <Link to='/contact'>contact</Link>
-            <Link to='/resume'>resume</Link>
-            </div>
-
-            <div class="logo"> logo </div>
+  // <div className="logo">
+  // <img src="https://icon-library.com/images/uppercase-latin-letter-round-j-keyboard-2-512.png" height="50" width="50" alt="logo"></img>
+  // </div>
+  return (
+    <div className=" ">
+      <div>
+        <div className="logo">
+          <h1 style={{ color: "#64ffda" }}>J</h1>
         </div>
-    );
+      </div>
+      <div>
+        <Link to="/about">about</Link>
+        <Link to="/contact">contact</Link>
+        <Link to="/resume">resume</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
